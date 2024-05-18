c11c35680f5c1a8b69e09513cc5d6db0cd484665
Luke Sloane-Bulger <luke.sloanebulger@protonmail.com>
2024-05-18 15:04:59 +0100

    backend - update

--- backend/src/routes/routes.ts
+++
@@ -1,7 +0,0 @@
-const controller = require("../controller/controller")
-
-exports.appRoutes = router => {
-    router.get("/", (req, res, next) => res.json({ test: "hi"}));
-    router.get("/getDeltas", controller.getDeltas);
-    router.get("/getSnapshot", controller.getSnapshot);
-}
--- backend/src/view/view.ts
+++
@@ -1,9 +0,0 @@
-exports.getDeltas = (deltas) => {
-    const lines = deltas.split(`\n`).map(line => line.split(','));
-    return { lines: lines }
-}
-
-exports.getSnapshot = (snapshotCsvData) => {
-    const [headers, ...stocks] = snapshotCsvData.split(`\n`);
-    return { headers: headers, stocks: stocks };
-}
